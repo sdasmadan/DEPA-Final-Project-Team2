e32f4410a10bb14798883176be7bfeeb74d69f85
Sam Gunther <sam.p.gunther@gmail.com>
2020-12-07 13:55:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from flask import escape
+import pandas as pd # table manipulation
+import numpy as np # number manipulation
+from datetime import datetime # time metrics
+#import re # string manipulation
+import usaddress as add # address parsing
+import configparser # read in MySQL connection attributes stored in a separate config file
+import pymysql # import mysql package to fetch data in Python
+from sqlalchemy import create_engine # SQL Package that helps connect to MySQL for DB Writes
 
 def hello_http(request):
     """HTTP Cloud Function.
@@ -20,3 +28,41 @@ def hello_http(request):
     else:
         name = 'World'
     return 'Hello {}!'.format(escape(name))
+    
+
+
+def inputData(limit=2000,offset=0,start_date="",condition=">="):
+    # Initiate df
+    columns = ['inspection_id', 'dba_name', 'aka_name', 'license_', 'facility_type',
+           'risk', 'address', 'city', 'state', 'zip', 'inspection_date',
+           'inspection_type', 'results', 'violations', 'latitude', 'longitude',
+           'location']
+    df = pd.DataFrame(columns=columns)
+    
+    # Input variables
+    limit = limit # Able to pull 50k at a time
+    offset = offset
+    counter = 1
+    rows = limit # Only to start while loop
+
+    # Need to subset data by Inspection Date?
+    if start_date != "":
+        start_filter = "&$where=inspection_date"+condition+"'"+start_date+"'"
+    else:
+        start_filter = start_date
+
+    start = datetime.now()
+
+    # Conduct while loop to paginate through dataset until all data for query retrieved
+    while limit == rows:
+        iter_start = datetime.now()
+        df_temp = pd.read_json("https://data.cityofchicago.org/resource/4ijn-s7e5.json?$limit="+
+                          str(limit)+"&$offset="+str(offset)+"&$order=inspection_date"+start_filter)
+        print("Time Between Iteration",counter,"-",datetime.now()-iter_start,"-",datetime.now()-start)
+        rows = df_temp.shape[0]
+        offset += limit
+        df = pd.concat([df,df_temp])
+        counter += 1
+    print("Finished")
+    
+    return df
