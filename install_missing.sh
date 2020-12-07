67d1c0fd9975226c6f21bcf9146d998101b4dd4f
Sam Gunther <sam.p.gunther@gmail.com>
2020-12-07 14:50:00 -0600

    Update install_missing.sh

--- install_missing.sh
+++ install_missing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y -qq --fix-missing install python3-mpltoolkits.basemap python3-numpy python3-matplotlib python3-requests
+sudo apt-get -y -qq --fix-missing install python3-pandas python3-numpy python3-datetime python3-re python3-usaddress python3-configparser python3-pymysql python3-create_engine
