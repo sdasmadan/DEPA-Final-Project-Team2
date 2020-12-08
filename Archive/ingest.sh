c37c3231610482a53e6e2b96f9e13e4ee7ee0266
Sam Gunther <sam.p.gunther@gmail.com>
2020-12-08 16:42:09 -0600

    Create ingest.sh

---
+++ Archive/ingest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install wget
+
+# remove older copy of file, if it exists
+rm -f foodinspectiondata.csv
+
+# download latest data from Chicago Data Portal
+wget https://data.cityofchicago.org/resource/4ijn-s7e5.csv -O /data/foodinspectiondata.csv
