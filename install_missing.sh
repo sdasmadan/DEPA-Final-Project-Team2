5a657971b971de04d1b22172a3c4967a6e7a1db8
Sam Gunther <sam.p.gunther@gmail.com>
2020-12-11 15:30:59 -0600

    Update install_missing.sh

--- install_missing.sh
+++ install_missing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y -qq --fix-missing install python3-pandas python3-numpy python3-datetime python3-re python3-usaddress python3-configparser python3-pymysql python3-create_engine
+sudo apt-get -y -qq --fix-missing install python3-pandas python3-numpy python3-datetime python3-re python3-usaddress python3-configparser python3-pymysql python3-sqlalchemy
